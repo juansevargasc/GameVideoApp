8298ad1a6c8393ccc658363d0287b73e78edd1dc
Juanse <jsvargasc@unal.edu.co>
2021-09-26 13:35:20 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Juanse Vargas on 26/09/21.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
